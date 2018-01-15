c8041832370f315868cc4db7a9f63f6d1ce32b5a
pengyanjun <pengyanjun@mfexcel.com>
2018-01-15 18:07:30 +0800

    testgit库中，handleNewTokenResponse修改日志

--- NetUtilV2.java
+++ NetUtilV2.java
@@ -181,7 +181,7 @@ public class NetUtilV2 {
     }
 
     private static void handleNewTokenResponse(JSONObject jsonObject) {
-		LogManager.e("pengyanjun", "handleNewTokenResponse:" + jsonObject);
+		LogManager.e("pengyanjun", "handleNewTokenResponse=" + jsonObject);
         if (jsonObject == null) {
             return;
         }
